ef76e627bd2834ffca21ad94a7a858d7da592581
TimTuerk <tim.tuerk@yahoo.de>
2025-04-21 01:26:35 +0200

    Many bugfixes, better zoom, fixed drawing cells wrong

--- Js/Generator.js
+++ Js/Generator.js
@@ -3,29 +3,58 @@ import {createSeededRandom} from "./Seed.js"
 let rng = createSeededRandom(17)
 
 
-export const ChunkList = [];
-export const chunkSize = 50
+export const ChunkList_posX_negY = [];
+export const ChunkList_posX_posY = [];
+export const ChunkList_negX_posY = [null];
+export const ChunkList_negX_negY = [null];
+export const chunkSize = 10
+export const ChunkGenDistance = 5 //generiert x chunks in jede richtung in rechteckform gesamte fläche 2x*2x 
 
-export function generateMap(){
-    for (let x = 0; x <= 100 ; x++) {
-        const yLine = []
-        for (let y = 0; y <= 100 ; y++) {
-            yLine.push(chunk())
-            //console.log("NEUER CHUNK");
-        }
-        ChunkList.push(yLine)
+
+function generateChunkRow(hasEntryInZero){
+    const yLine = []
+    if(hasEntryInZero === false){
+        yLine.push("null")
     }
-} 
+    for (let y = 0; y <= ChunkGenDistance ; y++) {
+        yLine.push(chunk())
+    }
+    return yLine    
+}
+
+export function generateAllChunks(){
+    for (let x = 0; x <= ChunkGenDistance ; x++) {
+        ChunkList_posX_negY.push(generateChunkRow(false))
+    }
+    for (let x = 0; x <= ChunkGenDistance ; x++) {
+        ChunkList_posX_posY.push(generateChunkRow(true))
+    }
+    for (let x = 0; x <= ChunkGenDistance ; x++) {
+        ChunkList_negX_posY.push(generateChunkRow(true))
+    }
+    for (let x = 0; x <= ChunkGenDistance ; x++) {
+        ChunkList_negX_negY.push(generateChunkRow(false))
+    }
+    /*for (let x = 0; x <= chunkSize*chunkSize-1 ; x++) {
+        changeChunkCell(ChunkList_posX_posY[0][0],x,2)
+    }*/
+    changeChunkCell(ChunkList_posX_posY[0][0],Math.round(chunkSize*chunkSize/2),2) // muss geamcht werden
+
+}
 
 function chunk(){
     const chunk = []
     let random = rng.nextInt(0, chunkSize*chunkSize-1)
     for (let x = 0; x <= chunkSize*chunkSize-1 ; x++) {
-        if (x == random){
+        if (x === random){
             chunk[x] = 1
         }else{
             chunk[x] = 0
         }  
     }
     return chunk
-} 
+}
+
+function changeChunkCell(array,cell,wert){
+    array[cell] = wert
+}
--- Js/main.js
+++ Js/main.js
@@ -8,18 +8,17 @@ ctx.canvas.height = window.innerHeight-50;
 ctx.fillStyle = "#ffffff";
 ctx.fillRect(0, 0, canvas.width,canvas.height);
 
+let lastColor = "#ffffff";
 
 let ratio = canvas.width / canvas.height;
-let CELL_COUNT_X = 100
+let CELL_COUNT_X = 500
 let CELL_COUNT_Y = Math.round(CELL_COUNT_X / ratio)
-
-
-export let chunkNumberx = Math.floor(CELL_COUNT_X/GEN.chunkSize)
-export let chunkNumbery = Math.floor(CELL_COUNT_Y/GEN.chunkSize)
+let CHUNK_COUNT_X = CELL_COUNT_X/ GEN.chunkSize
+let CHUNK_COUNT_Y = CELL_COUNT_Y/ GEN.chunkSize
 
 
 let CELL_PIXEL_X = canvas.width / CELL_COUNT_X
-let CELL_PIXEL_Y =  canvas.height / CELL_COUNT_Y
+let CELL_PIXEL_Y = canvas.height / CELL_COUNT_Y
 
 window.addEventListener('resize', function(event){
     ctx.canvas.width  = window.innerWidth-50;
@@ -34,62 +33,120 @@ window.addEventListener('resize', function(event){
 window.addEventListener("wheel", (event) => {
 
         CELL_COUNT_X = CELL_COUNT_X + Math.round(event.deltaY * 0.1)
-        CELL_COUNT_X = Math.min(Math.max(10, CELL_COUNT_X), 250);
+        CELL_COUNT_X = Math.min(Math.max(10, CELL_COUNT_X), 5000);
+
         calculateCell_Pixel()
+        
         drawMap()
 })
 
+//to-do: Namen ändern
 function calculateCell_Pixel(){
     ratio = canvas.width/canvas.height
     CELL_COUNT_Y = Math.round(CELL_COUNT_X / ratio)
     CELL_PIXEL_X = canvas.width / CELL_COUNT_X
     CELL_PIXEL_Y = canvas.height / CELL_COUNT_Y
 
-    chunkNumberx = Math.floor(CELL_COUNT_X/GEN.chunkSize)
-    chunkNumbery = Math.floor(CELL_COUNT_Y/GEN.chunkSize)
+    CHUNK_COUNT_X = CELL_COUNT_X/ GEN.chunkSize 
+    CHUNK_COUNT_Y = CELL_COUNT_Y/ GEN.chunkSize
 }
 
 
 
 
 function pixelPositionX(cellPositionInChunk,Chunk_pos_x){
-    return Chunk_pos_x * GEN.chunkSize *CELL_PIXEL_X +((cellPositionInChunk % GEN.chunkSize) * CELL_PIXEL_X)
+    let CC05 = CHUNK_COUNT_X/2
+    let real_pos = Chunk_pos_x + Math.floor(CC05)
+    let offset;
+
+    offset = CELL_PIXEL_X * GEN.chunkSize * ( CC05-Math.floor(CC05) ); //für halbe chunks
+   
+    return real_pos * GEN.chunkSize *CELL_PIXEL_X +(cellPositionInChunk % GEN.chunkSize) * CELL_PIXEL_X +offset - GEN.chunkSize/2 *CELL_PIXEL_X
 
 }
 function pixelPositionY(cellPositionInChunk,Chunk_pos_y){
-    return Chunk_pos_y * GEN.chunkSize * CELL_PIXEL_Y +( Math.floor(cellPositionInChunk/GEN.chunkSize)*CELL_PIXEL_Y)
+    let CC05 = CHUNK_COUNT_Y/2 
+    let real_pos = Chunk_pos_y + Math.floor(CC05)
+    let offset;
+    
+    offset = CELL_PIXEL_Y * GEN.chunkSize * ( CC05-Math.floor(CC05) ); //für halbe chunks
+
+    return real_pos * GEN.chunkSize * CELL_PIXEL_Y + Math.floor((cellPositionInChunk) / GEN.chunkSize)*CELL_PIXEL_Y +offset - GEN.chunkSize/2 *CELL_PIXEL_Y
 
 }
 
 function drawMap() {
-    for (let chunkx = 0; chunkx <= chunkNumberx; chunkx++) {
-        for (let chunky = 0; chunky <= chunkNumbery ; chunky++) {
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, canvas.width,canvas.height);
+    let chunkx = -Math.ceil(CHUNK_COUNT_X/2) //ist nur begrenzung das eigentliche drawn macht pixelposition
+    let chunkArray = GEN.ChunkList_posX_negY
+    while(chunkx <= Math.ceil(CHUNK_COUNT_X/2)){ // solange ich unter oder gleich 5 bin
+        let pchunkx = Math.abs(chunkx)
 
+
+        if(chunkx < -GEN.ChunkGenDistance){chunkx++; continue;} // wenn ich kleiner bin als -10 dan kein draw
+        if(chunkx > GEN.ChunkGenDistance){chunkx++; continue;} //wenn ich größer bin als 10 kein draw
+
+        let chunky = -Math.ceil(CHUNK_COUNT_Y/2)
+        while(chunky <= Math.ceil(CHUNK_COUNT_Y/2)){
+            
+            if(chunky < -GEN.ChunkGenDistance){chunky++;continue;}
+            if(chunky > GEN.ChunkGenDistance){chunky++;continue;}
+
+            //entscheide in welchem chunk-Array der chunk liegt
+            if(chunkx >= 0 && chunky >= 0){
+                chunkArray = GEN.ChunkList_posX_posY
+            }else if(chunkx >= 0 && chunky < 0){
+                chunkArray = GEN.ChunkList_posX_negY
+            }else if(chunkx < 0 && chunky >= 0){
+                chunkArray = GEN.ChunkList_negX_posY
+            }else if(chunkx < 0 && chunky < 0){
+                chunkArray = GEN.ChunkList_negX_negY
+            }else{console.log("ERROR"+ chunkx +"|"+ chunky)}
+
+            let pchunky = Math.abs(chunky)
             for(let cell = 0; cell <= GEN.chunkSize * GEN.chunkSize-1;cell++){
 
-                
-
-                if(GEN.ChunkList[chunkx][chunky][cell] == 0){
-                    ctx.fillStyle = "#181c28";
-                    ctx.fillRect(Math.ceil(pixelPositionX(cell,chunkx)),Math.ceil(pixelPositionY(cell,chunky)), Math.ceil(CELL_PIXEL_X),Math.ceil(CELL_PIXEL_Y)); 
-                }else{
-                    ctx.fillStyle = "#1969ff";
-                    ctx.fillRect(Math.ceil(pixelPositionX(cell,chunkx)), Math.ceil(pixelPositionY(cell,chunky)),  Math.ceil(CELL_PIXEL_X), Math.ceil(CELL_PIXEL_Y));
-                }
-
-                if((cell+1) % GEN.chunkSize == 0 || cell >= GEN.chunkSize * GEN.chunkSize-1-GEN.chunkSize){
-                    ctx.fillStyle = "#ff0000";
-                    ctx.fillRect(Math.ceil(pixelPositionX(cell,chunkx)),Math.ceil(pixelPositionY(cell,chunky)), Math.ceil(CELL_PIXEL_X),Math.ceil(CELL_PIXEL_Y));
-                }
-
+                fillCell(chunkArray,chunkx,chunky,cell,pchunkx,pchunky)
             }
-                        
+            chunky++            
         }
+        chunkx++
     }
 }
+ 
+function fillCell(chunkArray,chunkx,chunky,cell,pchunkx,pchunky){
+    let pixelpositionX = Math.ceil(pixelPositionX(cell,chunkx))
+    let pixelpositionY = Math.ceil(pixelPositionY(cell,chunky))
+
+    let value = chunkArray[pchunkx][pchunky][cell]
+    if(value === 0){
+        color("#181c28")
+        ctx.fillRect(pixelpositionX, pixelpositionY, Math.ceil(CELL_PIXEL_X),Math.ceil(CELL_PIXEL_Y)); 
+    }else if(value === 2){
+        color("#18ff28");
+        ctx.fillRect(pixelpositionX, pixelpositionY, Math.ceil(CELL_PIXEL_X),Math.ceil(CELL_PIXEL_Y));
+    }else{
+        color("#1969ff")
+        ctx.fillRect(pixelpositionX, pixelpositionY,  Math.ceil(CELL_PIXEL_X), Math.ceil(CELL_PIXEL_Y));
+    }
+
+    let x = cell % GEN.chunkSize;
+    let y = Math.floor(cell / GEN.chunkSize);
     
-    
+    if (x === GEN.chunkSize -1 || y === GEN.chunkSize -1) {
+        color("#ff0000")
+        ctx.fillRect(pixelpositionX,pixelpositionY, Math.ceil(CELL_PIXEL_X),Math.ceil(CELL_PIXEL_Y));
+    }
+}
+
+function color(newColor){
+    if (newColor !== lastColor){
+        ctx.fillStyle = newColor
+        lastColor = newColor
+    }
+}
 //main
-GEN.generateMap()
+GEN.generateAllChunks()
 drawMap()
 
